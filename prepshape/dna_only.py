3a1ad9c6442bf76813467b9d2f04eb312d884fda
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-06-25 16:50:04 -0700

    improved coverage in green regions, still need to get rid of circles and lines in image generated by dna_only

--- prepshape/dna_only.py
+++ prepshape/dna_only.py
@@ -1,5 +1,5 @@
 
-import cv2
+'''import cv2
 import numpy as np
 from prepshape import *
 
@@ -46,4 +46,51 @@ result = cv2.bitwise_or(result, dna_extracted)
 # Display the result
 cv2.imshow('Result', result)
 cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+
+import cv2
+import numpy as np
+from prepshape import *
+
+# Load the image
+prepped_image = image
+original_image = cv2.imread('dna.jpg')
+
+# Convert the image to the HSV color space
+hsv = cv2.cvtColor(prepped_image, cv2.COLOR_BGR2HSV)
+
+# Define the lower and upper bounds for the green color range
+lower_green = np.array([40, 50, 50])
+upper_green = np.array([80, 255, 255])
+
+# Threshold the HSV image to get only the green regions
+mask = cv2.inRange(hsv, lower_green, upper_green)
+
+# Perform morphological closing to fill gaps in the green regions
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+closed_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+# Perform morphological opening to remove small particles inside the DNA structure
+opened_mask = cv2.morphologyEx(closed_mask, cv2.MORPH_OPEN, kernel, iterations=2)
+
+# Apply the opened mask to the original image to get the DNA strand
+dna_extracted = cv2.bitwise_and(original_image, original_image, mask=opened_mask)
+
+# Create a white background image with the same size as the original image
+white_background = np.full_like(original_image, (255, 255, 255))
+
+# Perform background subtraction
+gray_image = cv2.cvtColor(dna_extracted, cv2.COLOR_BGR2GRAY)
+_, foreground_mask = cv2.threshold(gray_image, 1, 255, cv2.THRESH_BINARY_INV)
+
+# Replace the background with white using the foreground mask
+result = cv2.bitwise_or(white_background, white_background, mask=foreground_mask)
+
+# Combine the DNA extracted and the white background
+result = cv2.bitwise_or(result, dna_extracted)
+
+# Display the result
+cv2.imshow('Result', result)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
--- prepshape/prepshape.py
+++ prepshape/prepshape.py
@@ -1,4 +1,4 @@
-import cv2
+'''import cv2
 import numpy as np
 from data import *
 
@@ -38,5 +38,65 @@ for contour in contours:
 
 # Display the image
 cv2.imshow('Curvature Analysis', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+
+import cv2
+import numpy as np
+from data import *
+
+# Apply Gaussian blur to reduce noise
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# Perform thresholding to obtain a binary image
+_, binary = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+# Find contours in the binary image
+contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Set minimum contour area and aspect ratio thresholds
+min_contour_area = 17
+min_aspect_ratio = 0.2
+
+# Iterate over each contour
+for contour in contours:
+    # Calculate contour area
+    contour_area = cv2.contourArea(contour)
+
+    # Skip contours with small area
+    if contour_area < min_contour_area:
+        continue
+
+    # Calculate bounding rectangle for the contour
+    x, y, w, h = cv2.boundingRect(contour)
+
+    # Calculate aspect ratio of the bounding rectangle
+    aspect_ratio = float(w) / h
+
+    # Skip contours with low aspect ratio
+    if aspect_ratio < min_aspect_ratio:
+        continue
+
+    # Approximate the contour with fewer points
+    epsilon = 0.01 * cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+
+    # Fit a circle to the approximate contour
+    (cx, cy), radius = cv2.minEnclosingCircle(approx)
+    center = (int(cx), int(cy))
+    radius = int(radius)
+
+    # Check if the radius is zero
+    if radius > 0:
+        # Calculate curvature as the inverse of the radius
+        curvature = 1 / radius
+
+        # Draw the contour, bounding rectangle, and circle on the image
+        cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.circle(image, center, radius, (255, 0, 0), 2)
+
+# Display the image
+cv2.imshow('DNA Segmentation', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
