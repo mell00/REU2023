318cce98cc0d0387ef72d27807b1cd8dd06c1c6a
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-06-28 09:19:05 -0700

    added non-functional gui loading bar

---
+++ gui/gui_loadingbar.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import ttk
+import time
+
+def start_loading():
+    # Disable the button during loading
+    start_button.config(state=tk.DISABLED)
+    
+    # Simulate loading process
+    for i in range(101):
+        progress_bar["value"] = i
+        window.update_idletasks()
+        time.sleep(0.03)
+    
+    # Re-enable the button after loading
+    start_button.config(state=tk.NORMAL)
+
+# Create the main window
+window = tk.Tk()
+window.title("Loading Progress Bar")
+
+# Create a progress bar
+progress_bar = ttk.Progressbar(window, orient="horizontal", length=300, mode="determinate")
+progress_bar.pack(pady=20)
+
+# Create a button to start the loading process
+start_button = tk.Button(window, text="Start Loading", command=start_loading)
+start_button.pack(pady=10)
+
+# Start the Tkinter event loop
+window.mainloop()
