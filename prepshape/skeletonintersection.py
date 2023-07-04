ca037a8dead3fce4b89d4decc945959845e18fe4
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-07-04 14:47:06 -0700

    line intersection function that might help. Perhaps revise prepshape to skeletonize strands rather than outline in green?

---
+++ prepshape/skeletonintersection.py
@@ -0,0 +1,47 @@
+'''https://gist.github.com/jamjar919/05a76cf21035cef3cc86ac1979588d6d#file-skeletonintersection-py'''
+
+def getSkeletonIntersection(skeleton):
+    """ Given a skeletonised image, it will give the coordinates of the intersections of the skeleton.
+    
+    Keyword arguments:
+    skeleton -- the skeletonised image to detect the intersections of
+    
+    Returns: 
+    List of 2-tuples (x,y) containing the intersection coordinates
+    """
+    # A biiiiiig list of valid intersections             2 3 4
+    # These are in the format shown to the right         1 C 5
+    #                                                    8 7 6 
+    validIntersection = [[0,1,0,1,0,0,1,0],[0,0,1,0,1,0,0,1],[1,0,0,1,0,1,0,0],
+                         [0,1,0,0,1,0,1,0],[0,0,1,0,0,1,0,1],[1,0,0,1,0,0,1,0],
+                         [0,1,0,0,1,0,0,1],[1,0,1,0,0,1,0,0],[0,1,0,0,0,1,0,1],
+                         [0,1,0,1,0,0,0,1],[0,1,0,1,0,1,0,0],[0,0,0,1,0,1,0,1],
+                         [1,0,1,0,0,0,1,0],[1,0,1,0,1,0,0,0],[0,0,1,0,1,0,1,0],
+                         [1,0,0,0,1,0,1,0],[1,0,0,1,1,1,0,0],[0,0,1,0,0,1,1,1],
+                         [1,1,0,0,1,0,0,1],[0,1,1,1,0,0,1,0],[1,0,1,1,0,0,1,0],
+                         [1,0,1,0,0,1,1,0],[1,0,1,1,0,1,1,0],[0,1,1,0,1,0,1,1],
+                         [1,1,0,1,1,0,1,0],[1,1,0,0,1,0,1,0],[0,1,1,0,1,0,1,0],
+                         [0,0,1,0,1,0,1,1],[1,0,0,1,1,0,1,0],[1,0,1,0,1,1,0,1],
+                         [1,0,1,0,1,1,0,0],[1,0,1,0,1,0,0,1],[0,1,0,0,1,0,1,1],
+                         [0,1,1,0,1,0,0,1],[1,1,0,1,0,0,1,0],[0,1,0,1,1,0,1,0],
+                         [0,0,1,0,1,1,0,1],[1,0,1,0,0,1,0,1],[1,0,0,1,0,1,1,0],
+                         [1,0,1,1,0,1,0,0]];
+    image = skeleton.copy();
+    image = image/255;
+    intersections = list();
+    for x in range(1,len(image)-1):
+        for y in range(1,len(image[x])-1):
+            # If we have a white pixel
+            if image[x][y] == 1:
+                neighbours = zs.neighbours(x,y,image);
+                valid = True;
+                if neighbours in validIntersection:
+                    intersections.append((y,x));
+    # Filter intersections to make sure we don't count them twice or ones that are very close together
+    for point1 in intersections:
+        for point2 in intersections:
+            if (((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2) < 10**2) and (point1 != point2):
+                intersections.remove(point2);
+    # Remove duplicates
+    intersections = list(set(intersections));
+    return intersections;
