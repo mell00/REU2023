e63bdccd8ee6d40449b2629cde67a63d9537c817
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-07-05 10:58:33 -0700

    moved stuff around

--- prepskeleton/prepskeleton.py
+++ prepskeleton/prepskeleton.py
@@ -1,25 +1,39 @@
 from skimage.morphology import skeletonize
 from skimage.util import invert
+from skimage.util import img_as_ubyte
 import cv2
 import numpy as np
 from data import *
 
+min_size = 10
+
 # Apply Gaussian blur to reduce noise
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-#Thresholding
+# Perform thresholding to obtain a binary image
 _, binary = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-# Invert the binary image
-binary = invert(binary)
+# Skeletonize the binary image
+skeleton = skeletonize(binary / 255)
 
-# Perform skeletonization
-skeleton = skeletonize(binary)
+# Convert skeleton back to 8-bit image
+skeleton = img_as_ubyte(skeleton)
 
-# Convert back to BGR for displaying with OpenCV
-skeleton_bgr = cv2.cvtColor((skeleton * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+from skimage.measure import label, regionprops
+from skimage.morphology import remove_small_objects
 
-# Display the skeleton image
-cv2.imshow('DNA Segmentation', skeleton_bgr)
+# Convert binary image to boolean
+skeleton_bool = skeleton.astype(bool)
+
+# Remove objects smaller than the minimum size
+skeleton_bool = remove_small_objects(skeleton_bool, min_size, connectivity=2)
+
+# Convert boolean image back to 8-bit
+skeleton = img_as_ubyte(skeleton_bool)
+
+# Display the image
+cv2.imshow('DNA Segmentation', skeleton)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
