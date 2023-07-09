4f82670e089189f711cb08462ef9d8b47516b3cc
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-07-08 22:51:44 -0700

    added closing operation that fills gaps but merges noise in the process, needs more tuning (possibly multiple iterations of erosion/dilation)

--- prepskeleton/prepskeleton.py
+++ prepskeleton/prepskeleton.py
@@ -1,21 +1,37 @@
 from skimage.morphology import skeletonize
 from skimage.util import invert
 from skimage.util import img_as_ubyte
+from scipy import ndimage
 import cv2
 import numpy as np
 import os
 from data import *
+from sharpen import *
 
 min_size = 10
+
+
 
 # Apply Gaussian blur to reduce noise
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
+
+
 # Perform thresholding to obtain a binary image
 _, binary = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
+# Fill the holes
+from skimage.morphology import closing, square
+filled_image = closing(binary, square(4))
+
+# Display the original and filled images
+cv2.imshow('Filled Image', filled_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 # Skeletonize the binary image
-skeleton = skeletonize(binary / 255)
+skeleton = skeletonize(filled_image / 255)
 
 # Convert skeleton back to 8-bit image
 skeleton = img_as_ubyte(skeleton)
