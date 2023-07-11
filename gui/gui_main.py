06e7d5120279996a1666d087836fb8fe09162445
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-07-11 13:14:26 -0700

    fixed background and buttons

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -27,7 +27,7 @@ class KnotVision(tk.Tk):
         if self.frame is not None:
             self.frame.destroy()
         self.frame = new_frame
-        self.frame.pack()
+        self.frame.pack(fill="both", expand=True)
 
 
 class MainPage(tk.Frame):
@@ -46,83 +46,32 @@ class MainPage(tk.Frame):
         self.image_canvas = self.canvas.create_image(0, 0, anchor="nw", image=self.image_tk)
 
         # Bind the event handler to the canvas resize event
-        self.canvas.bind("<Configure>", self.resize_and_reposition_elements)
+        self.canvas.bind("<Configure>", self.resize_image)
 
         self.create_buttons()
 
-    def resize_and_reposition_elements(self, event=None):
-        self.resize_image()
-        self.update_button_positions()
-
     def resize_image(self, event=None):
-        # Get the current canvas size
-        canvas_width = self.canvas.winfo_width()
-        canvas_height = self.canvas.winfo_height()
+        canvas_width = event.width
+        canvas_height = event.height
 
-        # Check if canvas dimensions are non-zero and not minimized
-        if canvas_width > 1 and canvas_height > 1:
-            # Get the original image dimensions
-            original_width, original_height = self.front_image.size
-
-            # Calculate the scaling factor
-            width_ratio = canvas_width / original_width
-            height_ratio = canvas_height / original_height
-
-            # Choose the smaller ratio as the scaling factor
-            scaling_factor = min(width_ratio, height_ratio)
-
-            # Calculate the new dimensions
-            new_width = int(original_width * scaling_factor)
-            new_height = int(original_height * scaling_factor)
-
-            # Resize the image
-            front_image_resized = self.front_image.resize((new_width, new_height), Image.LANCZOS)
-
-            # Convert the resized PIL image into a Tkinter-compatible photo image
+        if canvas_width > 0 and canvas_height > 0:
+            front_image_resized = self.front_image.resize((canvas_width, canvas_height), Image.LANCZOS)
             self.image_tk = ImageTk.PhotoImage(front_image_resized)
-            
-            # Remove the old image and add the new one
-            self.canvas.delete(self.image_canvas)
-            self.image_canvas = self.canvas.create_image(0, 0, anchor="nw", image=self.image_tk)
-            
-            # Call the method to update the button positions
-            self.update_button_positions()
+            self.canvas.itemconfig(self.image_canvas, image=self.image_tk)
 
+    def create_buttons(self):
+        button1 = tk.Button(self, text="Run", height=2, width=5, command=self.nav_to_loading)
+        button1.place(relx=0.9, rely=0.6, anchor="center")
 
-    def update_button_positions(self):
-        # Get the current canvas size
-        canvas_width = self.canvas.winfo_width()
-        canvas_height = self.canvas.winfo_height()
+        button2 = tk.Button(self, text="Quit", height=2, width=5, command=open_file)
+        button2.place(relx=0.7, rely=0.8, anchor="center")
 
-        # Calculate new button positions
-        button1_x = canvas_width * 0.9  # 90% from left
-        button1_y = canvas_height * 0.6  # 60% from top
-
-        button2_x = canvas_width * 0.7  # 70% from left
-        button2_y = canvas_height * 0.8  # 80% from top
-
-        button3_x = canvas_width * 0.9  # 90% from left
-        button3_y = canvas_height * 0.8  # 80% from top
-
-        # Move buttons to new positions
-        self.button1.place(x=button1_x, y=button1_y)
-        self.button2.place(x=button2_x, y=button2_y)
-        self.button3.place(x=button3_x, y=button3_y)
-
+        button3 = tk.Button(self, text="Help", height=2, width=5, command=select_directory_and_convert)
+        button3.place(relx=0.9, rely=0.8, anchor="center")
 
 
     def nav_to_loading(self):
         subprocess.run(["python", "gui_loadingbar.py"])
-
-    def create_buttons(self):
-        self.button1 = tk.Button(self, text="Run", height=2, width=5, command=self.nav_to_loading)
-        self.button1_window = self.canvas.create_window(0, 0, window=self.button1, anchor='nw')
-
-        self.button2 = tk.Button(self, text="Quit", height=2, width=5, command=open_file)
-        self.button2_window = self.canvas.create_window(0, 0, window=self.button2, anchor='nw')
-
-        self.button3 = tk.Button(self, text="Help", height=2, width=5, command=select_directory_and_convert)
-        self.button3_window = self.canvas.create_window(0, 0, window=self.button3, anchor='nw')
 
 
 class RunPage(tk.Frame):
