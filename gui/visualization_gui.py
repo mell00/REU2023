791199dbb2923507385043516a389b4dca3da64f
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-07-11 13:45:30 -0700

    fixed exit button, need to implement everywhere else

--- gui/visualization_gui.py
+++ gui/visualization_gui.py
@@ -3,85 +3,100 @@ from PIL import Image, ImageTk
 import subprocess
 from uploadgui import *
 from convert_gui import *
+from gui_main import MainPage
 
-def create_canvas():
-    global canvas  # Declare the canvas variable as global
-    canvas = tk.Canvas(window)
-    canvas.pack(fill="both", expand=True)  # Use pack with fill and expand to fill the window
+import tkinter as tk
+from PIL import Image, ImageTk
 
-def resize_image(event=None):
-    global front_image, canvas, image_canvas
+class KnotVision(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
 
-    # Get the current canvas size
-    canvas_width = canvas.winfo_width()
-    canvas_height = canvas.winfo_height()
+        # Set up the main application window
+        self.title("DNA Image Recognition")
+        self.geometry('500x300')
+        self.minsize(682, 383)  # Set the minimum width and height
+        self.maxsize(683, 384)  # Set the maximum width and height
 
-    # Check if canvas dimensions are non-zero
-    if canvas_width > 0 and canvas_height > 0:
-        # Calculate the maximum size to fit within the canvas
-        max_width = canvas_width
-        max_height = canvas_height
+        ico = Image.open('dnaicon.ico')
+        photo = ImageTk.PhotoImage(ico)
+        self.wm_iconphoto(False, photo)
 
-        # Get the original image dimensions
-        original_width = front_image.width
-        original_height = front_image.height
+        self.frame = None
+        self.switch_frame(Main)
 
-        # Calculate the scaling factor
-        width_ratio = max_width / original_width
-        height_ratio = max_height / original_height
+    def switch_frame(self, target_frame):
+        new_frame = target_frame(self)
+        if self.frame is not None:
+            self.frame.destroy()
+        self.frame = new_frame
+        self.frame.pack(fill="both", expand=True)
 
-        # Choose the smaller ratio as the scaling factor
-        scaling_factor = min(width_ratio, height_ratio)
+class Main(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master  # Store the master for later use
 
-        # Calculate the new dimensions
-        new_width = int(original_width * scaling_factor)
-        new_height = int(original_height * scaling_factor)
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(fill="both", expand=True)
 
-        # Resize the image
-        front_image_resized = front_image.resize((new_width, new_height), Image.LANCZOS)  # Use LANCZOS instead of ANTIALIAS
+        # Load the image
+        self.front_image = Image.open("blankpage.png")
 
-        # Add the resized image to the canvas
-        image_tk = ImageTk.PhotoImage(front_image_resized)
-        canvas.itemconfig(image_canvas, image=image_tk)
-        canvas.image = image_tk  # Save a reference to prevent image garbage collection
+        # Add the original image to the canvas
+        self.image_tk = ImageTk.PhotoImage(self.front_image)
+        self.image_canvas = self.canvas.create_image(0, 0, anchor="nw", image=self.image_tk)
 
-window = tk.Tk()
-window.title("DNA Image Recognition")
-ico = Image.open('dnaicon.ico')
-photo = ImageTk.PhotoImage(ico)
-window.wm_iconphoto(False, photo)
-window.geometry('500x300')
+        # Bind the event handler to the canvas resize event
+        self.canvas.bind("<Configure>", self.resize_image)
 
-# Set the minimum and maximum size for the window
-window.minsize(682, 383)  # Set the minimum width and height
-window.maxsize(683, 384)  # Set the maximum width and height
+        self.create_buttons()
 
-def nav_to_loading():
-    subprocess.run(["python", "gui_loadingbar.py"])
+    def resize_image(self, event=None):
+        canvas_width = event.width
+        canvas_height = event.height
 
-def exit_to_main():
-    subprocess.run(["python", "gui_loadingbar.py"])
+        if canvas_width > 0 and canvas_height > 0:
+            front_image_resized = self.front_image.resize((canvas_width, canvas_height), Image.LANCZOS)
+            self.image_tk = ImageTk.PhotoImage(front_image_resized)
+            self.canvas.itemconfig(self.image_canvas, image=self.image_tk)
 
-def create_buttons():
-    window.geometry("450x350")
+    def create_buttons(self):
+        button1 = tk.Button(self, text="Exit", height=2, width=5, command=self.results_to_nav)
+        button1.place(relx=0.9, rely=0.6, anchor="center")
 
-    button1 = tk.Button(window, text="Exit", height= 2, width=5, command=open_file)
-    button1.place(x=420, y=192)
+        button2 = tk.Button(self, text="Quit", height=2, width=5, command=open_file)
+        button2.place(relx=0.7, rely=0.8, anchor="center")
 
-    button2 = tk.Button(window, text="Show image(s)", height= 2, width=5, command=nav_to_loading)
-    button2.place(x=341, y=267)
+        button3 = tk.Button(self, text="Help", height=2, width=5, command=select_directory_and_convert)
+        button3.place(relx=0.9, rely=0.8, anchor="center")
 
-create_canvas()
 
-# Load the image
-front_image = Image.open("blankpage.png")
+    def results_to_nav(self):
+        self.master.switch_frame(MainPage)  # Switch back to the main page of the application
 
-# Bind the event handler to the canvas resize event
-canvas.bind("<Configure>", resize_image)
 
-# Add the original image to the canvas
-image_tk = ImageTk.PhotoImage(front_image)
-image_canvas = canvas.create_image(0, 0, anchor="nw", image=image_tk)
+class ExitPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text='This is the Run page').pack()
+        tk.Button(self, text='Go back', command=lambda: master.switch_frame(MainPage)).pack()
 
-create_buttons()
-window.mainloop()
+
+class QuitPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text='This is the Quit page').pack()
+        tk.Button(self, text='Go back', command=lambda: master.switch_frame(MainPage)).pack()
+
+
+class HelpPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text='This is the Help page').pack()
+        tk.Button(self, text='Go back', command=lambda: master.switch_frame(MainPage)).pack()
+
+
+if __name__ == '__main__':
+    app = KnotVision()
+    app.mainloop()
