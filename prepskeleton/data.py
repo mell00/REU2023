72c4cbf4d9e18b430d56eda998cea5fa73532147
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-07-24 14:48:49 -0700

    this works but needs fine tuning

--- prepskeleton/data.py
+++ prepskeleton/data.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-image = cv2.imread('./dna.jpg')
+image = cv2.imread('./grid3_dna_5700.jpg')
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
---
+++ prepskeleton/test_image.py
@@ -0,0 +1,89 @@
+'''import cv2
+import numpy as np
+from scipy.ndimage import generic_filter
+
+def hysteresis_smoothing(image, threshold_low, threshold_high):
+    # Apply hysteresis thresholding
+    strong_pixels = (image >= threshold_high)
+    weak_pixels = (image >= threshold_low) & (image < threshold_high)
+
+    # Create an edge map to store the result
+    edge_map = np.zeros_like(image, dtype=np.uint8)
+
+    # Initialize the edge map with strong pixels
+    edge_map[strong_pixels] = 1
+
+    # Use a generic filter to find connected weak pixels
+    def hysteresis_filter(values):
+        return 1 if np.any(values) else 0
+
+    new_edge_pixels = generic_filter(edge_map, hysteresis_filter, size=(3, 3))
+
+    # Update the edge map with new weak pixels
+    edge_map[tuple(new_edge_pixels.squeeze().T)] = 1
+
+    return edge_map
+
+def detect_faint_wrinkles(image_path):
+    # Read the image
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # Define hysteresis thresholds (you can adjust these as needed)
+    threshold_low = 1
+    threshold_high = 3
+
+    # Apply hysteresis smoothing to detect faint wrinkles
+    edge_map = hysteresis_smoothing(image, threshold_low, threshold_high)
+
+    return edge_map
+
+if __name__ == "__main__":
+    image_path = "grid3_dna_5700.jpg"  # Replace with the path to your image
+    skeletonized_image = detect_faint_wrinkles(image_path)
+
+    # Save the skeletonized image
+    cv2.imwrite("skeletonized_image.jpg", (skeletonized_image * 255).astype(np.uint8))'''
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the image
+img = cv2.imread('grid3_dna_5700.jpg', 0)
+
+# Set filter strength (10 is a good starting point)
+filter_strength = 300
+
+# Apply Non-Local Means Denoising
+filtered_img = cv2.fastNlMeansDenoising(img, None, filter_strength, 7, 21)
+
+# Create a CLAHE object (Arguments are optional)
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+clahe_img = clahe.apply(filtered_img)
+
+# Apply morphological operations to enhance the creases
+#kernel = np.ones((5,5), np.uint8)
+#morph_img = cv2.morphologyEx(clahe_img, cv2.MORPH_OPEN, kernel)
+
+# Apply thresholding to make the background lighter using Otsu's Binarization
+#_, thresh_img = cv2.threshold(morph_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Display the original, denoised, CLAHE, Morphed and Thresholded images for comparison
+plt.figure(figsize=(24,6))
+plt.subplot(151), plt.imshow(img, cmap='gray'), plt.title('Original')
+plt.subplot(152), plt.imshow(filtered_img, cmap='gray'), plt.title('Denoised')
+plt.subplot(153), plt.imshow(clahe_img, cmap='gray'), plt.title('CLAHE Enhanced')
+#plt.subplot(154), plt.imshow(morph_img, cmap='gray'), plt.title('Morphology Enhanced')
+#plt.subplot(155), plt.imshow(thresh_img, cmap='gray'), plt.title('Thresholded')
+plt.show()
+
+
+
+
+
+
+
+
+
+
